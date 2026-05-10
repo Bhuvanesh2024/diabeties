264fc5e28121e8ecaa76c0b907a655791106dfb9
Bhuvanesh2024 <bhuvanesh.s2024aids@sece.ac.in>
2026-05-10 11:50:12 +0530

    Nice

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+from huggingface_hub import hf_hub_download
 
 app = Flask(__name__)
 
-model = joblib.load("random_forest_model.joblib")
-scaler = joblib.load("standard_scaler.joblib")
+repo_id = "bhuvanesh3602/Diabeties"
+model = joblib.load(hf_hub_download(repo_id=repo_id, filename="random_forest_model.joblib"))
+scaler = joblib.load(hf_hub_download(repo_id=repo_id, filename="standard_scaler.joblib"))
 
 @app.route("/predict", methods=["POST"])
 def predict():
